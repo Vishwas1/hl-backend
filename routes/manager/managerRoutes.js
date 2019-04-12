1650571e20556987c7a2e94f99a299d2a7e2492e
vishwas1 <vishwasanand.cse@gmail.com>
2019-04-12 15:36:01 +0530

    adding new route init

--- routes/manager/managerRoutes.js
+++ routes/manager/managerRoutes.js
@@ -3,7 +3,7 @@ const { InitServer, CarControllerBackEnd } =  require('../hl-connection')
 const { Car } = require('car-cc');
 
 InitServer();
-// user registration end point
+// http://localhost:3000/api/
 router.post('/', async (req, res) => {
   try{
     const newCar =  new Car({
@@ -16,10 +16,20 @@ router.post('/', async (req, res) => {
   }
 })
 
+// http://localhost:3000/api/init
+router.post('/init', async (req, res) => {
+  try{
+    await CarControllerBackEnd.init();
+    res.status(201).send();
+  }catch(err){
+    res.status(500).json(err);
+  }
+})
 
+// http://localhost:3000/api/?id=23
 router.get('/', async (req, res) => {
   try{
-    const car = new Car(await CarControllerBackEnd.query('10'));
+    const car = new Car(await CarControllerBackEnd.query(req.query.id));
     res.status(200).json(car)
   }catch(err){
     res.status(500).json(err);
