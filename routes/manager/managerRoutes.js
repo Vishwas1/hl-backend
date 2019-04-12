2dfb557ebd69a38a17b0cbb4ef185368467f9e50
vishwas1 <vishwasanand.cse@gmail.com>
2019-04-12 15:46:25 +0530

    adding route chaingeowner

--- routes/manager/managerRoutes.js
+++ routes/manager/managerRoutes.js
@@ -3,8 +3,8 @@ const { InitServer, CarControllerBackEnd } =  require('../hl-connection')
 const { Car } = require('car-cc');
 
 InitServer();
-// http://localhost:3000/api/
-router.post('/', async (req, res) => {
+// http://localhost:3000/api/create
+router.post('/create', async (req, res) => {
   try{
     const newCar =  new Car({
       id: '23', make: 'Vab', model: 'Model1', colour: 'blue', owner: 'Vishwas'
@@ -26,8 +26,18 @@ router.post('/init', async (req, res) => {
   }
 })
 
-// http://localhost:3000/api/?id=23
-router.get('/', async (req, res) => {
+// http://localhost:3000/api/changeOwner
+router.post('/changeOwner', async (req, res) => {
+  try{
+    await CarControllerBackEnd.changeOwner(req.body.id, req.body.owner);
+    res.status(201).send();
+  }catch(err){
+    res.status(500).json(err);
+  }
+})
+
+// http://localhost:3000/api/query?id=23
+router.get('/query', async (req, res) => {
   try{
     const car = new Car(await CarControllerBackEnd.query(req.query.id));
     res.status(200).json(car)
